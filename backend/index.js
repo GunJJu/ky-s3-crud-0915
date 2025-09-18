3507d8d3073b3f92b660e738f375500e0cdd8164
GunJJu <smkim0721@gmail.com>
2025-09-18 13:54:20 +0900

    .

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,25 @@
 import "dotenv/config";
 import express from "express"
 import cors from "cors"
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 import filesRouter from './routes/files.js'
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 await mongoose.connect(process.env.MONGO_URI)
 const app = express()
 
 app.use(cors({
-    origin: process.env.FRONT_ORIGIN,
-    credential: true
+    origin:process.env.FRONT_ORIGIN,
+    credentials:true
 }))
 
 app.use(express.json())
-app.use('/api/files', filesRouter)
+app.use('/api/files',filesRouter)
 
-app.get('/', (req, res) => {
-    res.send('hello world!')
+app.get('/',(req,res)=>{
+    res.send('Hello world')
 })
 
-app.listen(PORT, () => {
-    console.log(`server is running...! PORT : ${PORT}`)
+app.listen(PORT,()=>{
+    console.log(`Server is running ${PORT}`)
 })
